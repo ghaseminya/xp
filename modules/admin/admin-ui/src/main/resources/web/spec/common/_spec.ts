298ff292c467f79180b190cb919a595e6c2db657
Mikita Taukachou <edloidas@gmail.com>
2016-11-11 11:35:30 +0300

    Add ContentSummary tests

--- modules/admin/admin-ui/src/main/resources/web/spec/common/_spec.ts
+++ modules/admin/admin-ui/src/main/resources/web/spec/common/_spec.ts
@@ -1,2 +1,3 @@
 ///<reference path='ClassHelper.spec.ts' />
 ///<reference path='form/_spec.ts' />
+///<reference path='content/_spec.ts' />
---
+++ modules/admin/admin-ui/src/main/resources/web/spec/common/content/_spec.ts
@@ -0,0 +1,3 @@
+///<reference path='order/_spec.ts' />
+///<reference path='json/_spec.ts' />
+///<reference path='ContentSummary.spec.ts' />
